60d81f1b48bdf758c8e4416b943316f4add77b86
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2024-06-01 22:26:45 -0300

    test: create a lot of tests

--- config/database.ts
+++ config/database.ts
@@ -12,7 +12,7 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
-        ssl: false,
+        ssl: true,
       },
       migrations: {
         naturalSort: true,
